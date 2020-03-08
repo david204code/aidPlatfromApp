6712bb40cf5f22f11ea98c517b5e1b3e5a44789a
David Wu <david204code@gmail.com>
2020-03-08 22:10:51 +0000

    rendering json from the pages home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def home
+    render json: { status: "It's working" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   
   get 'pages/index'
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
