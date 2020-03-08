1576486930e635976cf7c254542dfd41d48fc1b2
David Wu <david204code@gmail.com>
2020-03-08 22:05:09 +0000

    config cors.rb, whitelist

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origin "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  credentials: true
+  end
+
+  #need to so the same for the domain of my production app, e.g heroku, just change the origin
+end 
