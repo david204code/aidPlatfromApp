6783a18fed808aa498c5567b07778be58c4a9882
David Wu <david204code@gmail.com>
2020-03-08 23:06:11 +0000

    Connected react to rails and build the registration component

---
+++ app/javascript/components/auth/Registration.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import axios from 'axios';
+
+class Registration extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      registrationErrors: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    axios
+    .post("http://localhost:3000/registrations", 
+    {
+      user: {
+        email: this.state.email,
+        password: this.state.password,
+        password_confirmation: this.state.password_confirmation
+      }
+    },
+    
+    { withCredentials: true }
+    ).then(response => {
+      if (response.data.status === 'created') {
+        this.props.handleSuccessfulAuth(response.data)
+      }
+    }).catch(error => {
+      console.log("registration error", error);
+    });
+    event.preventDefault();
+  }
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+          type="email" 
+          name="email" 
+          placeholder="Your Email" 
+          value={this.state.email} 
+          onChange={this.handleChange} 
+          required 
+          />
+
+          <input 
+          type="password"
+          name="password"
+          placeholder="Your password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          required
+          />
+
+          <input
+          type="password"
+          name="password_confirmation"
+          placeholder="Confirm password"
+          value={this.state.password_confirmation}
+          onChange={this.handleChange}
+          required
+          />
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Registration
