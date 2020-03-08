c459597194547962ca8373847543748d70630198
David Wu <david204code@gmail.com>
2020-03-08 22:48:19 +0000

    edited the session_store but need to be edited for production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: "_aidplatformapp" 
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_aidplatformapp" 
+  domain: "myheroku.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_aidplatformapp" 
+end
