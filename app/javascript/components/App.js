93989127449b4c46dfbc4aff0af702fa9499c208
David Wu <david204code@gmail.com>
2020-03-09 00:54:24 +0000

    checkLoginStatus but still work to be done

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -8,13 +8,38 @@ class App extends React.Component {
 
   constructor() {
     super();
-
-    this.state ={
+    
+    this.state = {
       loggedInStatus: "NOT_LOGGED_IN",
       user: {}
     }
 
-    this.handleLogin =this.handleLogin.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  checkLoginStatus() {
+    axios
+    .get("http://localhost:3000/logged_in", { withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    })
+    .catch(error => {
+      console.log("check login error", error);
+    });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
   handleLogin(data) {
