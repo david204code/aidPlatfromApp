29ddc8ba35e57317baee8b98d414328d86032bdb
David Wu <david204code@gmail.com>
2020-03-10 01:04:57 +0000

    yarn add react-map-gl, config webpack/environment.js and map along with marker are working

--- app/javascript/components/auth/Map.js
+++ app/javascript/components/auth/Map.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import {render} from 'react-dom';
+import MapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import ReactMapGL, {Marker} from 'react-map-gl';
+
+const TOKEN = 'pk.eyJ1IjoiZGF2aWQyMDRjb2RlMSIsImEiOiJjazc2YjdobGUwOTI0M2VvamwwZXpvZGR1In0.FSpShMuhbroEHA9-0iG4sg';
 
 class Map extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 51.508,
+        longitude: -0.140,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+      }
+    };
+  }
+
   render(){
     return (
-      <div>
-        <h1>Map Component</h1>
-      </div>
+      <ReactMapGL
+        {...this.state.viewport}
+        width="80vw"
+        height="80vh"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={viewport => this.setState({viewport})}
+        mapboxApiAccessToken={TOKEN}
+        >
+        <Marker 
+          latitude={51.508} 
+          longitude={-0.140} 
+          offsetLeft={-20} 
+          offsetTop={-10}
+          >
+          <button>You are here</button>
+        </Marker>
+      </ReactMapGL>
     );
   }
 }
