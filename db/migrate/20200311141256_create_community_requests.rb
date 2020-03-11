fa27ff43ee2e448de4b70e4c6172bacf9171720b
David Wu <david204code@gmail.com>
2020-03-11 14:16:19 +0000

    Added the CreateCommunityRequest attributes to its migration

--- db/migrate/20200311141256_create_community_requests.rb
+++ db/migrate/20200311141256_create_community_requests.rb
@@ -1,7 +1,14 @@
 class CreateCommunityRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :community_requests do |t|
-
+      t.string "title"
+      t.text "description"
+      t.string "request_type"
+      t.string "location_lat"
+      t.string "location_long"
+      t.string "status"
+      t.boolean "fulfilled", default: false 
+      
       t.timestamps
     end
   end
