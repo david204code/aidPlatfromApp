f56bac5ed9b32f7e9e101c02ccb55e30bc2cb48e
David Wu <david204code@gmail.com>
2020-03-15 22:39:59 +0000

    Move the map the Volunteering form component

--- app/javascript/components/community_request/Volunteering.js
+++ app/javascript/components/community_request/Volunteering.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import Map from '../auth/Map';
+import ReactMapGL, {Marker} from 'react-map-gl';
+import MapPin from '../auth/MapPin';
+import 'mapbox-gl/dist/mapbox-gl.css';
+// import Map from '../auth/Map';
+
+const TOKEN = 'pk.eyJ1IjoiZGF2aWQyMDRjb2RlMSIsImEiOiJjazc2YjdobGUwOTI0M2VvamwwZXpvZGR1In0.FSpShMuhbroEHA9-0iG4sg';
 
 class Volunteering extends React.Component {
   
@@ -13,7 +18,20 @@ class Volunteering extends React.Component {
       request_type: "",
       location_lat: "",
       location_long: "",
-      volunteeringErrors: ""
+      volunteeringErrors: "",
+
+      viewport: {
+        latitude: 51.508,
+        longitude: -0.140,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+      },
+      marker: {
+        latitude: 51.508,
+        longitude: -0.140
+      },
+      event: {}
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,7 +40,33 @@ class Volunteering extends React.Component {
 
   onClickMap(e) {
     console.log(e.lngLat);
+    var location_long = (e.lngLat[0]);
+    console.log({location_long});
   }
+
+  onDblClick(e) {
+    console.log("Hi", e.lngLat[0], e.lngLat[1]);
+  }
+
+  _logDragEvent(name, event) {
+    this.setState({
+      events: {
+        ...this.state.events,
+        [name]: event.lngLat
+      }
+    });
+  }
+
+  _onMarkerDragEnd = event => {
+    this._logDragEvent('onDragEnd', event);
+    this.setState({
+      marker: {
+        longitude: event.lngLat[0],
+        latitude: event.lngLat[1]
+      }
+    });
+    console.log("Longitude:",event.lngLat[0], "Latitude:", event.lngLat[1]);
+  };
 
   handleSubmit(event) {
     
@@ -52,14 +96,40 @@ class Volunteering extends React.Component {
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
-  }
-
+  }  
+  
   render() {
+    const {viewport, marker} = this.state;
     return (
       <div>
-        <Map />
+        <h1>Location longitude: {this.state.location_long}</h1>
+        {/* <Map /> */}
+        <ReactMapGL
+          {...this.state.viewport}
+          width="60vw"
+          height="60vh"
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          onViewportChange={viewport => this.setState({viewport})}
+          mapboxApiAccessToken={TOKEN}
+          onClick ={this.onClickMap}
+          onDblClick ={this.onDblClick}
+          doubleClickZoom ={false}
+          >
+          <Marker 
+            latitude={marker.latitude} 
+            longitude={marker.longitude} 
+            offsetLeft={-20} 
+            offsetTop={-10}
+            draggable
+            onDragStart={this._onMarkerDragStart}
+            onDrag={this._onMarkerDrag}
+            onDragEnd={this._onMarkerDragEnd}
+            >
+            <MapPin size={20} />
+          </Marker>
+        </ReactMapGL>
         <form onSubmit={this.handleSubmit}>          
           <input
             type ="text"
@@ -87,17 +157,17 @@ class Volunteering extends React.Component {
 
           <input
             type ="text"
-            name ="location_lat"
-            placeholder ="location_lat"
-            value ={this.state.location_lat}
+            name ="location_long"
+            placeholder ="location_long"
+            value ={this.state.location_long}
             onChange ={this.handleChange}
           />
 
           <input
             type ="text"
-            name ="location_long"
-            placeholder ="location_long"
-            value ={this.state.location_long}
+            name ="location_lat"
+            placeholder ="location_lat"
+            value ={this.state.location_lat}
             onChange ={this.handleChange}
           />
 
