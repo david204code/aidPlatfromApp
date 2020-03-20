d32a95bfa3d0d14f0f7c9868ea0b826e1f4f9af0
David Wu <david204code@gmail.com>
2020-03-20 17:31:22 +0000

    Got rails data to display on react

--- app/javascript/components/auth/Dashboard.js
+++ app/javascript/components/auth/Dashboard.js
@@ -15,6 +15,22 @@ class Dashboard extends React.Component {
     axios.get('/community_requests.json')
     .then(data => {
       // debugger
+      let res = []
+      data.data.data.map( (data) => {
+        res.push(
+          {
+            id: data.id, 
+            title: data.title, 
+            description: data.description, 
+            request_type: data.request_type, 
+            location_lat: data.location_lat, 
+            location_long: data.location_long
+
+          }
+        )
+
+        this.setState({community_requests: res})
+      })
     })
     .catch(data => {
       // debugger
@@ -22,9 +38,21 @@ class Dashboard extends React.Component {
   }
 
   render() {
+      var community_requests = this.state.community_requests.map((community_request) => {
+        return (
+          <div key ={community_request.id}>
+            <p>{community_request.title}</p>
+            <p>{community_request.description}</p>
+            <p>{community_request.request_type}</p>
+            <p>{community_request.location_lat}</p>
+            <p>{community_request.location_long}</p>
+          </div>
+        )
+      })
+
     return (
       <div>
-        Hello
+        {community_requests}
       </div>
     )
   }
